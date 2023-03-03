7521286adbaaf5112729da1de82c7034c844a7b5
Jayden Deller-Daoust <jayden.dellerdaoust@gmail.com>
2023-03-03 18:10:02 -0500

    UI Optimizations

--- ProjectScarf/Assets/Scripts/Health_Bar.cs
+++ ProjectScarf/Assets/Scripts/Health_Bar.cs
@@ -23,29 +23,6 @@ public class Health_Bar : MonoBehaviour
     {
         int hp = combat.hp;
 
-        switch(hp)
-        {
-            case 6:
-                healthBar.GetComponent<Image>().sprite = healthSprites[6];
-                break;
-            case 5:
-                healthBar.GetComponent<Image>().sprite = healthSprites[5];
-                break;
-            case 4:
-                healthBar.GetComponent<Image>().sprite = healthSprites[4];
-                break;
-            case 3:
-                healthBar.GetComponent<Image>().sprite = healthSprites[3];
-                break;
-            case 2:
-                healthBar.GetComponent<Image>().sprite = healthSprites[2];
-                break;
-            case 1:
-                healthBar.GetComponent<Image>().sprite = healthSprites[1];
-                break;
-            case 0:
-                healthBar.GetComponent<Image>().sprite = healthSprites[0];
-                break;
-        }
+        healthBar.GetComponent<Image>().sprite = healthSprites[hp];
     }
 }
